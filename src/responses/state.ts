e75fb196ad0fe2a5367ad79f9e4b545d50e187e6
Benliu <476212964@qq.com>
2026-09-01 16:31:46 +0800

    test: 稳定高负载发布门禁

--- src/responses/state.ts
+++ src/responses/state.ts
@@ -1885,11 +1885,14 @@ export function inspectAbandonedResponseStateTemps(): ResponseStateTempRecoveryR
 }
 
 /** Sweeper adapter: narrows the reclaim to the `() => number` the liveness tick expects. */
-export function sweepAbandonedResponseStateTemps(): number {
+export function sweepAbandonedResponseStateTemps(
+  options: ResponseStateTempRecoveryOptions = {},
+): number {
   return reclaimAbandonedResponseStateTemps({
     maxEntries: PERIODIC_TEMP_MAX_ENTRIES,
     maxCleanups: PERIODIC_TEMP_MAX_CLEANUPS,
     deadlineMs: PERIODIC_TEMP_SCAN_DEADLINE_MS,
+    ...options,
   }).removed;
 }
 
